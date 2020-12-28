82962cfd5a25277256c3c184ffeb25326db3581a
Michał Wiosna <michal.wiosna93@gmail.com>
2020-12-28 19:03:24 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const firstName = 'Michal';
+const age = '27';
+
+console.log(firstName);
+console.log(age);
+alert('Cześć! Witam Cię na mojej stronie szkoleniowej, którą tworzę podczas kursu "WTF Co ten Frontend"')
+
+console.log(`Nazywam sie ${firstName} i mam  ${age} lat`);
