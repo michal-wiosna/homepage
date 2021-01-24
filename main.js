05cd07f8d0a6a057cc0b640b68a44222441d905c
Michał Wiosna <michal.wiosna93@gmail.com>
2021-01-24 18:45:05 +0100

    changes

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-const firstName = 'Michal';
-const age = '27';
-
+const firstName = "Michal";
+const age = "27";
+ 
 console.log(firstName);
 console.log(age);
-alert('Cześć! Witam Cię na mojej stronie szkoleniowej, którą tworzę podczas kursu "WTF Co ten Frontend"')
+//alert(
+//  'Cześć! Witam Cię na mojej stronie szkoleniowej, którą tworzę podczas kursu "WTF Co ten Frontend'
+//);
 
 console.log(`Nazywam sie ${firstName} i mam  ${age} lat`);
+
+
+
+const heading = document.querySelector('.main-heading--js');
+
+heading.innerHTML = 'Nazywam sie ${firstName} i mam ${age} lat'
+console.log(heading);
