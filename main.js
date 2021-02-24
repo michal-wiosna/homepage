d2e423b68440b235cf2ec87d99d85c004d8e04ea
Michał Wiosna <michal.wiosna93@gmail.com>
2021-02-24 20:24:29 +0100

    start of 5th week course of 1 and 2 lesson

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const firstName = "Michal";
 const age = "27";
- 
+
 console.log(firstName);
 console.log(age);
 //alert(
@@ -9,9 +9,25 @@ console.log(age);
 
 console.log(`Nazywam sie ${firstName} i mam  ${age} lat`);
 
+const heading = document.querySelector(".main__heading--js");
+
+heading.innerHTML = `Nazywam sie ${firstName} i mam  ${age} lat`;
+// console.log(heading);
+
+const emptyParagraph = document.querySelector(".main__about--me--js");
+emptyParagraph.innerHTML = `Uzupełnienie treści JavaScriptem`;
 
 
-const heading = document.querySelector('.main__heading--js');
+function calculate(myNumber) {
+  console.log(`Otrzymalem ${myNumber}`);
+  myNumber = myNumber * 7;
+  return myNumber;
+}
+const myResult = calculate(10);
+console.log(`A teraz otrzymalem ${myResult} po wykonaniu dzialania`);
 
-heading.innerHTML = 'Nazywam sie ${firstName} i mam  ${age} lat'
-console.log(heading);
+
+function greet(age, firstName) {
+  console.log(`Nazywam sie ${firstName} i mam  ${age} lat`);
+}
+greet(28, "Michwisn");
